0b1dab7bf187186c84def433098d27a7479788c5
mizayeyo <b.mizayeyo@gmail.com>
2024-07-31 10:48:00 +0900

    11-502

---
+++ src/com/mycompany/ch04/For2.java
@@ -0,0 +1,41 @@
+package com.mycompany.ch04;
+
+public class While {
+
+	public static void main(String[] args) {
+		
+		//1부터 10까지 더하는 프로그램 작성
+		
+//		int sum = 0;
+//		int i = 1;
+//		
+//		while(i <=10) {
+//			sum += i;
+//			System.out.println(i + ": sum: " + sum);
+//			i++;
+//		}
+//
+//		System.out.println("sum " + sum);
+//		
+//		
+//		}
+		
+		int sum = 0;
+		for(int i=11; i <=50; i++) {
+			if(i%2==0) {
+			sum += i;
+		}else {
+			
+		}
+		}		
+		
+		System.out.println(sum);
+		
+	}
+	}
+	
+
+
+	
+
+
---
+++ src/com/mycompany/ch04/While.java
@@ -0,0 +1,17 @@
+package com.mycompany.ch04;
+
+public class While {
+
+	public static void main(String[] args) {
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+	}
+}
