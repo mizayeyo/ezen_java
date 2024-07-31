2948bdcd87757e580c0b355357d2c97b39c5d6c8
mizayeyo <b.mizayeyo@gmail.com>
2024-07-31 10:08:27 +0900

    2-9

---
+++ src/com/mycompany/ch04/For1.java
@@ -0,0 +1,31 @@
+package com.mycompany.ch04;
+
+public class For1 {
+
+	public static void main(String[] args) {
+		
+		
+		//for(int a=1; a<=3; a++) {
+		//	System.out.println("a: "+ a);
+		//}
+
+		//System.out.println("------------------------");
+		//for(int a=; a<=3; a++) {
+			//System.out.println("a: "+ a);
+		//}
+		
+		for(int i=2; i<=9; i++) {
+			for(int j=1; j<=9; j++) { 
+			System.out.println(i + " x " + j + " = " + i*j);
+			
+			}
+		
+		if( i !=9)
+		System.out.println("------------------------");
+		
+	}
+  }		
+}	
+	
+
+
